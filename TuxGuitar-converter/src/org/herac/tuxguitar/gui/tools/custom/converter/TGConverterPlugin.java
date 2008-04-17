87a3ce94e86235329bfc920683aa6f2ef03b3415
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 21:58:42 +0000

    Initial import.

---
+++ TuxGuitar-converter/src/org/herac/tuxguitar/gui/tools/custom/converter/TGConverterPlugin.java
@@ -0,0 +1,32 @@
+package org.herac.tuxguitar.gui.tools.custom.converter;
+
+public class TGConverterPlugin extends org.herac.tuxguitar.gui.system.plugins.base.TGToolItemPlugin {
+
+	// "TGConverterDialog" reference don't should exists when plugin isn't used
+	// private TGConverterDialog dialog = null;
+	
+	protected void doAction() {
+		new TGConverterDialog().show();
+	}
+
+	protected String getItemName() {
+		return "File format batch converter";
+	}
+
+	public String getName() {
+		return "BatchConverter";
+	}
+	
+	public String getAuthor() {
+		return "Nikola Kolarovic & Julian Casadesus";
+	}
+
+	public String getDescription() {
+		return "Converts folder containing various tab formats into wanted file format.\n" +
+			   "Depending on your loaded file format plugins, you can read and write.";
+	}
+
+	public String getVersion() {
+		return "0.2b";
+	}
+}
