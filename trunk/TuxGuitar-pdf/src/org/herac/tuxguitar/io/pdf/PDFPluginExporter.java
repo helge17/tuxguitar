d901b37ff6db5cb74a6ed14b8a7e1ebee6638f18
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 22:17:58 +0000

    Initial import.

---
+++ trunk/TuxGuitar-pdf/src/org/herac/tuxguitar/io/pdf/PDFPluginExporter.java
@@ -0,0 +1,31 @@
+package org.herac.tuxguitar.io.pdf;
+
+import org.herac.tuxguitar.gui.system.plugins.base.TGExporterPlugin;
+import org.herac.tuxguitar.io.base.TGSongExporter;
+
+public class PDFPluginExporter extends TGExporterPlugin{
+	
+	protected TGSongExporter getExporter() {
+		return new PDFSongExporter();
+	}
+
+	public String getVersion() {
+		return "1.0-rc3";
+	}	
+
+	public String getAuthor() {
+		return "Julian Casadesus <julian@casadesus.com.ar>";
+	}
+
+	public String getName() {
+		return "PDF file format exporter";
+	}
+
+	public String getDescription() {
+		String description = new String();
+		description += ("TuxGuitar-pdf is an \"iText\" based *.pdf exporter plugin.");
+		description += ("\niText Homepage: http://www.lowagie.com/iText/index.html");
+		return description;
+	}	
+
+}
