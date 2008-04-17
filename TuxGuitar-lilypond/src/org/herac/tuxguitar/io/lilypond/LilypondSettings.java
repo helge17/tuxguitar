13e51fa08cb831060c8c2bb2129b04805d25ddb1
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 22:10:41 +0000

    Initial import.

---
+++ TuxGuitar-lilypond/src/org/herac/tuxguitar/io/lilypond/LilypondSettings.java
@@ -0,0 +1,74 @@
+package org.herac.tuxguitar.io.lilypond;
+
+public class LilypondSettings {
+
+	public static final int ALL_TRACKS = -1;
+	
+	public static final int FIRST_MEASURE = -1;
+	
+	public static final int LAST_MEASURE = -1;
+	
+	private int track;
+
+	private int measureFrom;
+	
+	private int measureTo;
+
+	private boolean trackNameEnabled;
+	
+	private boolean trackGroupEnabled;
+	
+	public LilypondSettings(){
+		super();
+	}
+	
+	public int getMeasureFrom() {
+		return this.measureFrom;
+	}
+
+	public void setMeasureFrom(int measureFrom) {
+		this.measureFrom = measureFrom;
+	}
+
+	public int getMeasureTo() {
+		return this.measureTo;
+	}
+
+	public void setMeasureTo(int measureTo) {
+		this.measureTo = measureTo;
+	}
+
+	public int getTrack() {
+		return this.track;
+	}
+
+	public void setTrack(int track) {
+		this.track = track;
+	}
+
+	public boolean isTrackGroupEnabled() {
+		return this.trackGroupEnabled;
+	}
+
+	public void setTrackGroupEnabled(boolean trackGroupEnabled) {
+		this.trackGroupEnabled = trackGroupEnabled;
+	}
+
+	public boolean isTrackNameEnabled() {
+		return this.trackNameEnabled;
+	}
+
+	public void setTrackNameEnabled(boolean trackNameEnabled) {
+		this.trackNameEnabled = trackNameEnabled;
+	}
+
+	public static LilypondSettings getDefaults(){
+		LilypondSettings settings = new LilypondSettings();
+		settings.setTrack(ALL_TRACKS);
+		settings.setMeasureFrom(FIRST_MEASURE);
+		settings.setMeasureTo(LAST_MEASURE);
+		settings.setTrackGroupEnabled(true);
+		settings.setTrackNameEnabled(false);
+		return settings;
+	}
+}
---
+++ TuxGuitar-lilypond/src/org/herac/tuxguitar/io/lilypond/LilypondSongExporter.java
@@ -0,0 +1,32 @@
+package org.herac.tuxguitar.io.lilypond;
+
+import java.io.OutputStream;
+
+import org.herac.tuxguitar.io.base.TGFileFormat;
+import org.herac.tuxguitar.io.base.TGSongExporter;
+import org.herac.tuxguitar.song.models.TGSong;
+
+public class LilypondSongExporter implements TGSongExporter{
+
+	private LilypondSettings settings;
+	
+	public String getExportName() {
+		return "Lilypond";
+	}
+
+	public TGFileFormat getFileFormat() {
+		return new TGFileFormat("Lilypond","*.ly");
+	}	
+	
+	public boolean configure(boolean setDefaults) {
+		this.settings = (setDefaults ? LilypondSettings.getDefaults() : new LilypondSettingsDialog().open());
+		return (this.settings != null);
+	}	
+	
+	public void exportSong(OutputStream stream, TGSong song) {
+		if(this.settings != null){
+			new LilypondOutputStream(stream,this.settings).writeSong(song);
+		}
+	}
+
+}
