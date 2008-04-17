d50a0f7af662ccd7bb95bf167e08d50cc0dfb2f6
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 21:53:29 +0000

    s Initial import.

---
+++ trunk/TuxGuitar-ascii/src/org/herac/tuxguitar/io/ascii/ASCIIPluginExporter.java
@@ -0,0 +1,28 @@
+package org.herac.tuxguitar.io.ascii;
+
+import org.herac.tuxguitar.gui.system.plugins.base.TGExporterPlugin;
+import org.herac.tuxguitar.io.base.TGSongExporter;
+
+public class ASCIIPluginExporter extends TGExporterPlugin{
+	
+	protected TGSongExporter getExporter() {
+		return new ASCIISongExporter();
+	}
+	
+	public String getAuthor() {
+		return "Julian Casadesus <julian@casadesus.com.ar>";
+	}
+
+	public String getDescription() {		
+		return "ASCII tab exporter";
+	}
+
+	public String getName() {
+		return "ASCII tab exporter";
+	}
+
+	public String getVersion() {
+		return "1.0-rc3";
+	}
+
+}
