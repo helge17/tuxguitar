686f0a1b2815d267bcc3b67893ad59f0c5037e84
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 22:01:57 +0000

    Initial import.

---
+++ TuxGuitar-fluidsynth/src/org/herac/tuxguitar/player/impl/midiport/fluidsynth/MidiOutImpl.java
@@ -0,0 +1,43 @@
+package org.herac.tuxguitar.player.impl.midiport.fluidsynth;
+
+import org.herac.tuxguitar.player.base.MidiControllers;
+import org.herac.tuxguitar.player.base.MidiOut;
+
+public class MidiOutImpl implements MidiOut{
+	
+	private MidiSynth synth;
+	
+	public MidiOutImpl(MidiSynth synth){
+		this.synth = synth;
+	}
+	
+	public void sendSystemReset() {
+		this.synth.sendSystemReset();
+	}
+	
+	public void sendNoteOn(int channel, int key, int velocity) {
+		this.synth.sendNoteOn(channel, key, velocity);
+	}
+		
+	public void sendNoteOff(int channel, int key, int velocity) {
+		this.synth.sendNoteOff(channel, key, velocity);
+	}
+	
+	public void sendControlChange(int channel, int controller, int value) {
+		this.synth.sendControlChange(channel, controller, value);
+	}
+	
+	public void sendProgramChange(int channel, int value) {
+		this.synth.sendProgramChange(channel, value);
+	}
+
+	public void sendPitchBend(int channel, int value) {
+		this.synth.sendPitchBend(channel, value);
+	}
+		
+	public void sendAllNotesOff() {
+		for(int i = 0; i < 16; i ++){
+			this.sendControlChange(i,MidiControllers.ALL_NOTES_OFF,0);
+		}
+	}
+}
---
+++ TuxGuitar-fluidsynth/src/org/herac/tuxguitar/player/impl/midiport/fluidsynth/MidiPortImpl.java
@@ -0,0 +1,59 @@
+package org.herac.tuxguitar.player.impl.midiport.fluidsynth;
+
+import java.io.File;
+
+import org.herac.tuxguitar.player.base.MidiOut;
+import org.herac.tuxguitar.player.base.MidiPort;
+import org.herac.tuxguitar.player.impl.midiport.fluidsynth.MidiOutImpl;
+
+public class MidiPortImpl extends MidiPort{
+
+	private MidiSynth synth;
+	private MidiOutImpl midiOut;
+	private String soundFont;
+	
+	public MidiPortImpl(MidiSynth synth,File soundfont){
+		super(getUniqueKey(soundfont),getUniqueName(soundfont));
+		this.soundFont = soundfont.getAbsolutePath();
+		this.midiOut = new MidiOutImpl(synth);
+		this.synth = synth;
+	}
+	
+	public void open(){
+		if(!this.synth.isConnected(this)){
+			this.synth.connect(this);
+		}
+	}
+	
+	public void close(){
+		if(this.synth.isConnected(this)){
+			this.synth.disconnect(this);
+		}
+	}
+	
+	public MidiOut out(){
+		this.open();
+		return this.midiOut;
+	}
+	
+	public void check(){
+		// Not implemented
+	}
+	
+	public String getSoundFont() {
+		return this.soundFont;
+	}
+	
+	public static String getUniqueKey(File soundfont){
+		return ("tuxguitar-fluidsynth_" + soundfont.getAbsolutePath());
+	}
+	
+	private static String getUniqueName(File soundfont){
+		String name = soundfont.getName();
+		int extensionIndex = name.lastIndexOf(".");
+		if( extensionIndex > 0 ){
+			name = name.substring( 0, extensionIndex );
+		}
+		return ("TG Fluidsynth " + "[" + name + "]");
+	}
+}
