6eaaa540ab45c77bb36013137e7c8533b124db16
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 21:29:32 +0000

    Initial import.

---
+++ trunk/TuxGuitar-tray/src/org/herac/tuxguitar/tray/TGTrayIcon.java
@@ -0,0 +1,33 @@
+package org.herac.tuxguitar.tray;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.TrayItem;
+import org.herac.tuxguitar.gui.util.TGFileUtils;
+
+public class TGTrayIcon {
+
+	private Image image;
+	private TrayItem item;
+	
+	public TGTrayIcon(){
+		super();
+	}
+	
+	public void setItem(TrayItem item){
+		this.item = item;
+	}
+
+	public void loadImage(){
+		this.dispose();
+		if(this.item != null && !this.item.isDisposed()){
+			this.image = TGFileUtils.loadImage("icon-24x24.png");
+			this.item.setImage(this.image);
+		}
+	}
+	
+	public void dispose(){
+		if(this.image != null && !this.image.isDisposed()){
+			this.image.dispose();
+		}
+	}
+}
