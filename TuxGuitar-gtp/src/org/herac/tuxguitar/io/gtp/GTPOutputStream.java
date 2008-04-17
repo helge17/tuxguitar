ce7943c16ec93c4cd3acb4845517c6855a38e766
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 22:06:32 +0000

    Initial import.

---
+++ TuxGuitar-gtp/src/org/herac/tuxguitar/io/gtp/GTPOutputStream.java
@@ -0,0 +1,80 @@
+package org.herac.tuxguitar.io.gtp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.herac.tuxguitar.io.base.TGOutputStreamBase;
+import org.herac.tuxguitar.song.factory.TGFactory;
+
+public abstract class GTPOutputStream implements TGOutputStreamBase{
+
+    private TGFactory factory;
+    private OutputStream outputStream;
+
+	public void init(TGFactory factory,OutputStream stream) {
+		this.factory = factory;
+		this.outputStream = stream;
+	}     
+    
+	protected TGFactory getFactory(){
+		return this.factory;
+	}
+	
+    protected void skipBytes(int count) throws IOException {
+    	for(int i = 0;i < count;i++){
+    		this.outputStream.write(0);
+    	}
+    }
+
+    protected void writeByte(byte v) throws IOException {
+        this.outputStream.write(v);
+    }
+
+    protected void writeUnsignedByte(int v) throws IOException {
+        this.outputStream.write(v);
+    }
+    
+    protected void writeBytes(byte[] v) throws IOException {
+        this.outputStream.write(v);
+    }
+
+    protected void writeBoolean(boolean v) throws IOException {
+        this.outputStream.write(v ? 1 : 0);
+    }    
+    
+    protected void writeInt(int v) throws IOException {
+        byte[] bytes = { (byte)(v & 0x00FF),(byte)((v >> 8) & 0x000000FF),(byte) ((v >> 16) & 0x000000FF),(byte)((v >> 24) & 0x000000FF) };
+        this.outputStream.write(bytes);
+    }    
+
+    protected void writeString(char[] chars, int maximumLength) throws IOException {
+    	int length = (maximumLength == 0 || maximumLength > chars.length ? chars.length : maximumLength );
+    	for(int i = 0 ; i < length; i ++){
+    		this.outputStream.write( chars[ i ] );
+    	}
+    }
+
+    protected void writeStringInteger(String string) throws IOException {
+    	char[] chars = string.toCharArray();
+    	this.writeInt( chars.length );
+        this.writeString( chars , 0 );
+    }
+    
+    protected void writeStringByte(String string,int size) throws IOException {
+    	char[] chars = string.toCharArray();
+    	this.writeByte( (byte)( size == 0 || size > chars.length ? chars.length : size ));
+        this.writeString( chars , size );
+        this.skipBytes( size - chars.length );
+    }
+
+    protected void writeStringByteSizeOfInteger(String string) throws IOException {
+    	char[] chars = string.toCharArray();
+    	this.writeInt( (chars.length + 1) );
+        this.writeStringByte(string,chars.length);
+    }
+
+    protected void close() throws IOException{
+        this.outputStream.flush();
+        this.outputStream.close();
+    }
+}
---
+++ TuxGuitar-gtp/src/org/herac/tuxguitar/io/gtp/GTPPluginList.java
@@ -0,0 +1,75 @@
+package org.herac.tuxguitar.io.gtp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.herac.tuxguitar.gui.system.plugins.base.TGInputStreamPlugin;
+import org.herac.tuxguitar.gui.system.plugins.base.TGOutputStreamPlugin;
+import org.herac.tuxguitar.gui.system.plugins.base.TGPluginList;
+import org.herac.tuxguitar.io.base.TGInputStreamBase;
+import org.herac.tuxguitar.io.base.TGOutputStreamBase;
+
+public class GTPPluginList extends TGPluginList{
+
+	protected List getPlugins() {
+		List plugins = new ArrayList();
+		plugins.add(new TGInputStreamPlugin() {
+			protected TGInputStreamBase getInputStream() {
+				return new GP5InputStream();
+			}
+		});
+		plugins.add(new TGInputStreamPlugin() {
+			protected TGInputStreamBase getInputStream() {
+				return new GP4InputStream();
+			}
+		});
+		plugins.add(new TGInputStreamPlugin() {
+			protected TGInputStreamBase getInputStream() {
+				return new GP3InputStream();
+			}
+		});		
+		plugins.add(new TGInputStreamPlugin() {
+			protected TGInputStreamBase getInputStream() {
+				return new GP2InputStream();
+			}
+		});		
+		plugins.add(new TGInputStreamPlugin() {
+			protected TGInputStreamBase getInputStream() {
+				return new GP1InputStream();
+			}
+		});
+		plugins.add(new TGOutputStreamPlugin() {
+			protected TGOutputStreamBase getOutputStream() {
+				return new GP5OutputStream();
+			}
+		});
+		plugins.add(new TGOutputStreamPlugin() {
+			protected TGOutputStreamBase getOutputStream() {
+				return new GP4OutputStream();
+			}
+		});
+		plugins.add(new TGOutputStreamPlugin() {
+			protected TGOutputStreamBase getOutputStream() {
+				return new GP3OutputStream();
+			}
+		});
+		return plugins;
+	}
+
+	public String getAuthor() {
+		return "Julian Casadesus <julian@casadesus.com.ar>";
+	}
+
+	public String getName() {
+		return "GPx File Format plugin";
+	}
+
+	public String getDescription() {		
+		return "GPx File Format plugin for TuxGuitar";
+	}
+		
+	public String getVersion() {
+		return "1.0-rc3";
+	}	
+	
+}
