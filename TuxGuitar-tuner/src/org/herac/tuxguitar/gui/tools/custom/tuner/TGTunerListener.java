c115fddb34bd6e15a0741ec162b2d3ec49ed65a3
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 22:22:42 +0000

    Initial import.

---
+++ TuxGuitar-tuner/src/org/herac/tuxguitar/gui/tools/custom/tuner/TGTunerListener.java
@@ -0,0 +1,11 @@
+package org.herac.tuxguitar.gui.tools.custom.tuner;
+
+public interface TGTunerListener {
+
+	public void fireFrequency(double freq);
+	
+	public int[] getTuning();
+	
+	public void fireException(Exception ex);
+	
+}
