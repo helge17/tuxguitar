f3f8ed0b1218a5c8b9fa1e12944999212811302b
akdmia <akdmia@e5ace225-184b-0410-aeed-e0aa2ff36a70>
2008-04-17 21:55:21 +0000

    Initial import.

---
+++ trunk/TuxGuitar-browser-ftp/src/org/herac/tuxguitar/gui/tools/browser/ftp/TGBrowserFactoryImpl.java
@@ -0,0 +1,132 @@
+package org.herac.tuxguitar.gui.tools.browser.ftp;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.herac.tuxguitar.gui.TuxGuitar;
+import org.herac.tuxguitar.gui.tools.browser.base.TGBrowser;
+import org.herac.tuxguitar.gui.tools.browser.base.TGBrowserData;
+import org.herac.tuxguitar.gui.tools.browser.base.TGBrowserFactory;
+import org.herac.tuxguitar.gui.util.DialogUtils;
+
+public class TGBrowserFactoryImpl implements TGBrowserFactory{
+
+	public TGBrowserFactoryImpl() {
+		super();
+	}
+
+	public String getType(){
+		return "ftp";
+	}
+
+	public String getName(){
+		return "FTP";
+	}
+	
+	public TGBrowser newTGBrowser(TGBrowserData data) {
+		if(data instanceof TGBrowserDataImpl){
+			return new TGBrowserImpl((TGBrowserDataImpl)data);
+		}
+		return null;
+	}
+
+	public TGBrowserData parseData(String string) {
+		return TGBrowserDataImpl.fromString(string);
+	}
+
+	public TGBrowserData dataDialog(Shell parent) {
+		return new TGBrowserDataDialog().show(parent);
+	}
+	
+}
+class TGBrowserDataDialog{
+	
+	protected TGBrowserDataImpl data;
+	
+	public TGBrowserDataImpl show(Shell parent){
+		final Shell dialog = DialogUtils.newDialog(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+		
+        dialog.setLayout(new GridLayout());
+        dialog.setText(TuxGuitar.getProperty("FTP Location"));
+        
+        //-------------LIBRARY DATA-----------------------------------------------
+        Composite composite = new Composite(dialog, SWT.NONE);
+        composite.setLayout(new GridLayout(2,false));
+        composite.setLayoutData(new GridData(SWT.FILL,SWT.FILL,true,true));   
+        
+        GridData textData = new GridData(SWT.FILL,SWT.FILL,true,true);
+        textData.minimumWidth = 300;
+        
+        //host
+        Label hostLabel = new Label(composite, SWT.NULL);
+        hostLabel.setText(TuxGuitar.getProperty("Host"));
+        final Text hostText = new Text(composite,SWT.BORDER);
+        hostText.setLayoutData(textData);
+
+        //path
+        Label pathLabel = new Label(composite, SWT.NULL);
+        pathLabel.setText(TuxGuitar.getProperty("Path"));
+        final Text pathText = new Text(composite,SWT.BORDER);
+        pathText.setLayoutData(textData);        
+        
+        //user
+        Label userLabel = new Label(composite, SWT.NULL);
+        userLabel.setText(TuxGuitar.getProperty("Login name"));
+        final Text userText = new Text(composite,SWT.BORDER);
+        userText.setLayoutData(textData);
+        
+        //password
+        Label passwordLabel = new Label(composite, SWT.NULL);
+        passwordLabel.setText(TuxGuitar.getProperty("Password"));
+        final Text passwordText = new Text(composite,SWT.BORDER | SWT.PASSWORD);
+        passwordText.setLayoutData(textData);
+        
+        //------------------BUTTONS--------------------------            
+        Composite buttons = new Composite(dialog, SWT.NONE);
+        buttons.setLayout(new GridLayout(2,false));
+        buttons.setLayoutData(new GridData(SWT.END,SWT.FILL,true,true));    	
+        
+        GridData data = new GridData(SWT.FILL,SWT.FILL,true,true);    
+        data.minimumWidth = 80;
+        data.minimumHeight = 25;            
+                
+        final Button buttonOk = new Button(buttons, SWT.PUSH);
+        buttonOk.setText(TuxGuitar.getProperty("ok"));
+        buttonOk.setLayoutData(data);
+        buttonOk.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent arg0) {   
+                String host = hostText.getText();
+                String path = pathText.getText();
+                String user = userText.getText();
+                String password = passwordText.getText();
+                if(host != null && host.length() > 0){
+                	TGBrowserDataDialog.this.data = new TGBrowserDataImpl(host,path,user,password);
+                }
+            	dialog.dispose();
+            }
+        });
+
+        Button buttonCancel = new Button(buttons, SWT.PUSH);
+        buttonCancel.setLayoutData(data);
+        buttonCancel.setText(TuxGuitar.getProperty("cancel"));
+        buttonCancel.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent arg0) {
+                dialog.dispose();
+            }
+        });
+
+        dialog.setDefaultButton( buttonOk );
+        
+        DialogUtils.openDialog(dialog,DialogUtils.OPEN_STYLE_CENTER | DialogUtils.OPEN_STYLE_PACK | DialogUtils.OPEN_STYLE_WAIT);
+		
+		return this.data;
+	}
+	
+}
---
+++ trunk/TuxGuitar-browser-ftp/src/org/herac/tuxguitar/gui/tools/browser/ftp/TGBrowserImpl.java
@@ -0,0 +1,158 @@
+package org.herac.tuxguitar.gui.tools.browser.ftp;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.herac.tuxguitar.gui.tools.browser.TGBrowserException;
+import org.herac.tuxguitar.gui.tools.browser.base.TGBrowser;
+import org.herac.tuxguitar.gui.tools.browser.base.TGBrowserElement;
+
+import sun.net.ftp.FtpClient;
+
+public class TGBrowserImpl extends TGBrowser{
+
+	private TGBrowserDataImpl data;
+	private String root; 
+	private String path; 
+	private FtpClient client;
+			
+	public TGBrowserImpl(TGBrowserDataImpl data){
+		this.data = data;
+	}
+	
+	private String getRoot(){
+		if(this.root == null){
+			this.root = "/";			
+			if(this.data.getPath() != null && this.data.getPath().length() > 0){
+				this.root = this.data.getPath();
+			}						
+		}
+		return this.root;
+	}
+	
+	public void open() throws TGBrowserException{
+		try {
+			this.client = new FtpClient();
+			this.client.openServer(this.data.getHost());
+			this.client.login(this.data.getUsername(),this.data.getPassword());			
+			this.cdRoot();
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}	
+	}
+	
+	public void close() throws TGBrowserException{
+		try {
+			this.client.closeServer();
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+	}
+	
+	public void cdElement(TGBrowserElement element) throws TGBrowserException {			
+		try {
+			this.client.cd(element.getName());
+			this.path = this.client.pwd();
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+	}
+
+	public void cdRoot() throws TGBrowserException {
+		try {
+			this.client.cd(getRoot());
+			this.path = this.client.pwd();
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+	}
+
+	public void cdUp() throws TGBrowserException {		
+		try {
+			this.client.cdUp();
+			this.path = this.client.pwd();
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+	}
+
+	public List listElements() throws TGBrowserException {		
+		List elements = new ArrayList();
+		try {
+			this.client.ascii();
+			String[] names = parseString(this.client.nameList(this.path)).split("\n");
+			String[] infos = parseString(this.client.list()).split("\n");		
+			if(names.length > 0 && infos.length > 0){
+				for(int i = 0;i < names.length;i++){
+					String name = names[i].trim();
+					
+					if(name.indexOf(this.path) == 0 && name.length() > (this.path.length() + 2)){
+						name = name.substring(this.path.length() + 1);
+					}
+					
+					for(int j = 0;j < infos.length;j++){
+						String info = infos[j].trim();
+						if(info.indexOf(name) > 0){
+							elements.add(new TGBrowserElementImpl(this,name,info,this.path));
+							break;
+						}
+					}
+				}				
+			}
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+		return elements;
+	}
+		
+	public InputStream getInputStream(String path,TGBrowserElement element)throws TGBrowserException {		
+		try {			
+			this.client.cd(path);
+			this.client.binary();
+			
+			byte[] bytes = getByteBuffer( this.client.get(element.getName()) , false );
+			
+			this.client.ascii();
+			
+			return new ByteArrayInputStream( bytes );
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+	}
+
+	private String parseString(InputStream in) throws TGBrowserException{
+		try {
+			byte[] bytes = getByteBuffer(in, true);
+			
+			return new String( bytes );
+		} catch (Throwable throwable) {
+			throw new TGBrowserException(throwable);
+		}
+	}	
+
+	private byte[] getByteBuffer(InputStream in, boolean checkAvailable) throws IOException{
+		if(!checkAvailable || in.available() > 0){
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+			int read = 0;
+			while((read = in.read()) != -1){
+				out.write(read);					
+			}
+			
+			byte[] bytes = out.toByteArray();
+
+			in.close();
+			out.close();
+			out.flush();
+		
+			return bytes;
+		}
+		in.close();
+		
+		return new byte[0];
+	}
+}
