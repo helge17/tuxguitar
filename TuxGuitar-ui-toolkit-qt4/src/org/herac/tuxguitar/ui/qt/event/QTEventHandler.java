1757114eaf6c3541a22ce8b5916497a84d7f6925
helge17 <5897631+helge17@users.noreply.github.com>
2023-03-11 22:00:13 +0100

    Remove QT4

--- TuxGuitar-ui-toolkit-qt4/src/org/herac/tuxguitar/ui/qt/event/QTEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.herac.tuxguitar.ui.qt.event;
-
-import com.trolltech.qt.core.QEvent;
-
-public interface QTEventHandler {
-	
-	boolean handle(QEvent event);
-}
--- TuxGuitar-ui-toolkit-qt4/src/org/herac/tuxguitar/ui/qt/event/QTSignalHandler.java
+++
@@ -1,8 +0,0 @@
-package org.herac.tuxguitar.ui.qt.event;
-
-public interface QTSignalHandler {
-	
-	String SIGNAL_METHOD = "handle()";
-	
-	void handle();
-}
