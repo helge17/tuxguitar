7e5eca30072bd5fcdb154f9b85d8ab9cec69b03f
helge17 <5897631+helge17@users.noreply.github.com>
2023-03-21 11:38:16 +0100

    Tweak tuxguitar.sh for MacOS and tuxguitar.bat for Windows

--- build-scripts/common-resources/common-macosx/Contents/MacOS/tuxguitar.sh
+++ build-scripts/common-resources/common-macosx/Contents/MacOS/tuxguitar.sh
@@ -1,44 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 ##SCRIPT DIR
-DIR_NAME=`dirname "$0"`
-DIR_NAME=`cd "$DIR_NAME"; pwd`
-cd "${DIR_NAME}"
+TG_DIR=`dirname "$0"`
+TG_DIR=`cd "$TG_DIR"; pwd`
+cd "${TG_DIR}"
 ##JAVA
-JAVA_BUNDLED="./jre/bin/java"
-if [ -f "${JAVA_BUNDLED}" ]; then
-	JAVA="${JAVA_BUNDLED}"
-fi
-if [ -z ${JAVA} ]; then
-	[ -z ${JAVA_HOME} ] && JAVA_HOME=$(/usr/libexec/java_home)
-	[ ! -f "${JAVA}" ] && JAVA="${JAVA_HOME}/bin/java"
-	[ ! -f "${JAVA}" ] && JAVA="java"
-fi
-##CLASSPATH
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-ui-toolkit.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-ui-toolkit-swt.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-lib.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-editor-utils.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-gm-utils.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-awt-graphics.jar
-CLASSPATH=${CLASSPATH}:./lib/swt.jar
-CLASSPATH=${CLASSPATH}:./lib/gervill.jar
-CLASSPATH=${CLASSPATH}:./lib/itext-pdf.jar
-CLASSPATH=${CLASSPATH}:./lib/itext-xmlworker.jar
-CLASSPATH=${CLASSPATH}:./lib/commons-compress.jar
-CLASSPATH=${CLASSPATH}:./share/
-CLASSPATH=${CLASSPATH}:./dist/
+JAVA=`which java`
 ##LIBRARY_PATH
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:lib/
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TG_DIR}/lib/
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/jni
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+##CLASSPATH
+CLASSPATH=${CLASSPATH}:${TG_DIR}/lib/*
+CLASSPATH=${CLASSPATH}:${TG_DIR}/share/
+CLASSPATH=${CLASSPATH}:${TG_DIR}/dist/
 ##MAINCLASS
 MAINCLASS=org.herac.tuxguitar.app.TGMainSingleton
 ##JVM ARGUMENTS
 VM_ARGS="-Xmx512m"
 ##SWT ARGUMENTS
 SWT_ARGS="-XstartOnFirstThread"
+##EXPORT VARS
+export CLASSPATH
+export LD_LIBRARY_PATH
 ##LAUNCH
-exec "${JAVA}" ${VM_ARGS} ${SWT_ARGS} -cp :${CLASSPATH} -Dtuxguitar.home.path="${DIR_NAME}" -Djava.library.path="${LD_LIBRARY_PATH}" ${MAINCLASS} "$1" "$2"
+"${JAVA}" ${VM_ARGS} ${SWT_ARGS} -cp :${CLASSPATH} -Dtuxguitar.home.path="${TG_DIR}" -Djava.library.path="${LD_LIBRARY_PATH}" ${MAINCLASS} "$@"
