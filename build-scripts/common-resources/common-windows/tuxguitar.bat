7d25685baf77dcc57a7442ef15e64baa18c27886
helge17 <5897631+helge17@users.noreply.github.com>
2023-04-11 18:58:47 +0200

    Fix tuxguitar.exe and .bat: TuxGuitar now starts correctly when double-clicking on a .tg file in Explorer or when dropping a file on the .exe or .bat file.

--- build-scripts/common-resources/common-windows/tuxguitar.bat
+++ build-scripts/common-resources/common-windows/tuxguitar.bat
@@ -1,4 +1,8 @@
-cd "."
+@ECHO OFF
+
+SET "TG_START=%cd%"
+
+cd %~dp0
 
 SET "TG_HOME=."
 SET "TG_MAIN_CLASS=org.herac.tuxguitar.app.TGMainSingleton"
@@ -12,3 +16,5 @@ SET "JAVA_CLASSPATH=%JAVA_CLASSPATH%;share\"
 SET "JAVA_CLASSPATH=%JAVA_CLASSPATH%;dist\"
 
 %JAVA% -cp %JAVA_CLASSPATH% -Dtuxguitar.home.path=%TG_HOME% -Djava.library.path=%JAVA_LIBRARY_PATH% %TG_MAIN_CLASS% %1 %2 %3 %4 %5 %6 %7 %8 %9 %10
+
+cd %TG_START%
