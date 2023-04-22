9dc67b10dde2698ef2117d413ad8222628877c12
helge17 <5897631+helge17@users.noreply.github.com>
2023-04-22 10:17:56 +0200

    Add translation for plugins tools.file-format-batch-converter tools.tuner - https://sourceforge.net/p/tuxguitar/patches/51/

--- TuxGuitar-converter/src/org/herac/tuxguitar/app/tools/custom/converter/TGConverterPlugin.java
+++ TuxGuitar-converter/src/org/herac/tuxguitar/app/tools/custom/converter/TGConverterPlugin.java
@@ -1,20 +1,21 @@
 package org.herac.tuxguitar.app.tools.custom.converter;
 
+import org.herac.tuxguitar.app.TuxGuitar;
 import org.herac.tuxguitar.util.TGContext;
 
 
 public class TGConverterPlugin extends org.herac.tuxguitar.app.tools.custom.TGToolItemPlugin {
-	
+
 	public static final String MODULE_ID = "tuxguitar-converter";
-	
+
 	protected void doAction(TGContext context) {
 		new TGConverterDialog(context).show();
 	}
-	
+
 	protected String getItemName() {
-		return "File format batch converter";
+		return TuxGuitar.getProperty("tools.file-format-batch-converter");
 	}
-	
+
 	public String getModuleId(){
 		return MODULE_ID;
 	}
