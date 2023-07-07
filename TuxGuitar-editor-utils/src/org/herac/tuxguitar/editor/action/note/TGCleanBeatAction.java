04b77293292209314f95c08b24c8a7f2ff3f81e7
guiv42 <129443524+guiv42@users.noreply.github.com>
2023-07-07 22:29:09 +0200

    apply actions to selection: set note duration, clean beat
    essentially backported from 2.0beta fork, but adapted
    to keep compatibility with Android version:
    if the NOTE_RANGE and/or BEAT_RANGE attributes are not
    defined in action context or are empty,
    then keep original implementation

--- TuxGuitar-editor-utils/src/org/herac/tuxguitar/editor/action/note/TGCleanBeatAction.java
+++ TuxGuitar-editor-utils/src/org/herac/tuxguitar/editor/action/note/TGCleanBeatAction.java
@@ -4,6 +4,7 @@ import org.herac.tuxguitar.action.TGActionContext;
 import org.herac.tuxguitar.document.TGDocumentContextAttributes;
 import org.herac.tuxguitar.editor.action.TGActionBase;
 import org.herac.tuxguitar.song.models.TGBeat;
+import org.herac.tuxguitar.util.TGBeatRange;
 import org.herac.tuxguitar.util.TGContext;
 
 public class TGCleanBeatAction extends TGActionBase {
@@ -15,9 +16,17 @@ public class TGCleanBeatAction extends TGActionBase {
 	}
 	
 	protected void processAction(TGActionContext context){
-		TGBeat beat = ((TGBeat) context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_BEAT));
-		if( beat != null ){
-			getSongManager(context).getMeasureManager().cleanBeat(beat);
+		TGBeatRange beats = (TGBeatRange) context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_BEAT_RANGE);
+		
+		if (beats!=null && !beats.isEmpty()){
+			for (TGBeat beat : beats.getBeats()) {
+				getSongManager(context).getMeasureManager().cleanBeat(beat);
+			}
+		} else {
+			TGBeat beat = (TGBeat) context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_BEAT);
+			if( beat != null ){
+				getSongManager(context).getMeasureManager().cleanBeat(beat);
+			}
 		}
 	}
 }
