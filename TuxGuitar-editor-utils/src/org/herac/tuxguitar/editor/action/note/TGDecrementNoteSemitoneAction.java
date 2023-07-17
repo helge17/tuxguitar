24c53ca574a6e59d12b45e815b542909168c0152
guiv42 <129443524+guiv42@users.noreply.github.com>
2023-07-17 22:47:38 +0200

    increment/decrement semitone: all notes or none
    it does not make sense to apply action on some notes but not all of them

--- TuxGuitar-editor-utils/src/org/herac/tuxguitar/editor/action/note/TGDecrementNoteSemitoneAction.java
+++ TuxGuitar-editor-utils/src/org/herac/tuxguitar/editor/action/note/TGDecrementNoteSemitoneAction.java
@@ -7,12 +7,17 @@ import org.herac.tuxguitar.song.models.TGNote;
 import org.herac.tuxguitar.util.TGContext;
 
 public class TGDecrementNoteSemitoneAction extends TGTransposeNoteSemitoneAction {
+	
 	public static final String NAME = "action.note.general.decrement-semitone";
 
 	public TGDecrementNoteSemitoneAction(TGContext context) {
 		super(context, NAME);
 	}
 
+	@Override
+	protected boolean canTransposeSemiTone(TGMeasureManager measureManager, TGMeasure measure, TGBeat beat, TGNote note) {
+		return measureManager.canMoveSemitoneDown(measure, beat.getStart(), note.getString());
+	}
 	@Override
 	protected boolean transposeSemiTone(TGMeasureManager measureManager, TGMeasure measure, TGBeat beat, TGNote note) {
 		return measureManager.moveSemitoneDown(measure, beat.getStart(), note.getString());
