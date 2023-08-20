1f221651c6248f84e783cedbe08493ffe2e9fd48
guiv42 <129443524+guiv42@users.noreply.github.com>
2023-08-20 20:15:28 +0200

    invert Decrement/Increment Duration Actions
    to be consistent with "+" and "-" default shortcuts:
    "increment duration" shall increment the duration from the user's point of
    view: less notes within 1 single measure
    
    with this modification, Androip app behavior becomes consistent: arrow up
    increases note duration
    
    also invert icons in FretBoard, MatrixEditor and Piano to keep actual behavior:
    arrow up increases note duration

--- TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/piano/TGPiano.java
+++ TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/piano/TGPiano.java
@@ -122,16 +122,16 @@ public class TGPiano {
 		this.createToolSeparator(uiFactory, ++column);
 		
 		// duration
-		this.decrement = uiFactory.createButton(this.toolComposite);
-		this.decrement.addSelectionListener(new TGActionProcessorListener(this.context, TGDecrementDurationAction.NAME));
-		this.createToolItemLayout(this.decrement, ++column);
+		this.increment = uiFactory.createButton(this.toolComposite);
+		this.increment.addSelectionListener(new TGActionProcessorListener(this.context, TGIncrementDurationAction.NAME));
+		this.createToolItemLayout(this.increment, ++column);
 		
 		this.durationLabel = uiFactory.createImageView(this.toolComposite);
 		this.createToolItemLayout(this.durationLabel, ++column);
 		
-		this.increment = uiFactory.createButton(this.toolComposite);
-		this.increment.addSelectionListener(new TGActionProcessorListener(this.context, TGIncrementDurationAction.NAME));
-		this.createToolItemLayout(this.increment, ++column);
+		this.decrement = uiFactory.createButton(this.toolComposite);
+		this.decrement.addSelectionListener(new TGActionProcessorListener(this.context, TGDecrementDurationAction.NAME));
+		this.createToolItemLayout(this.decrement, ++column);
 		
 		// separator
 		this.createToolSeparator(uiFactory, ++column);
@@ -560,8 +560,8 @@ public class TGPiano {
 	public void loadIcons(){
 		this.goLeft.setImage(TuxGuitar.getInstance().getIconManager().getArrowLeft());
 		this.goRight.setImage(TuxGuitar.getInstance().getIconManager().getArrowRight());
-		this.decrement.setImage(TuxGuitar.getInstance().getIconManager().getArrowUp());
-		this.increment.setImage(TuxGuitar.getInstance().getIconManager().getArrowDown());
+		this.decrement.setImage(TuxGuitar.getInstance().getIconManager().getArrowDown());
+		this.increment.setImage(TuxGuitar.getInstance().getIconManager().getArrowUp());
 		this.settings.setImage(TuxGuitar.getInstance().getIconManager().getSettings());
 		this.loadDurationImage(true);
 		this.control.layout();
