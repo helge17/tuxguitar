helge17/tuxguitar

commit 531495044d70a7e93afb29049527325cfbb7d8c4

initialize start/end of training mode with selected measures
to test:
- select some measures
- type F9 (or menu Player/play mode)
loop range is initialized with measures from selection

TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/transport/TGTransportModeDialog.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.herac.tuxguitar.app.TuxGuitar;
 import org.herac.tuxguitar.app.action.impl.transport.TGTransportModeAction;
 import org.herac.tuxguitar.app.ui.TGApplication;
+import org.herac.tuxguitar.app.view.component.tab.TablatureEditor;
 import org.herac.tuxguitar.app.view.controller.TGViewContext;
 import org.herac.tuxguitar.app.view.util.TGDialogUtil;
 import org.herac.tuxguitar.document.TGDocumentContextAttributes;
@@ -29,6 +30,7 @@ import org.herac.tuxguitar.ui.widget.UIRadioButton;
 import org.herac.tuxguitar.ui.widget.UISelectItem;
 import org.herac.tuxguitar.ui.widget.UISpinner;
 import org.herac.tuxguitar.ui.widget.UIWindow;
+import org.herac.tuxguitar.util.TGBeatRange;
 
 public class TGTransportModeDialog {
 	
@@ -199,6 +201,12 @@ public class TGTransportModeDialog {
 		this.loopEHeader = uiFactory.createDropDownSelect(rangeGroup);
 		rangeLayout.set(this.loopEHeader, 2, 2, UITableLayout.ALIGN_FILL, UITableLayout.ALIGN_FILL, true, true);
 		mHeaderRangeStatus.addControl(this.loopEHeader);
+		
+		final TGBeatRange beats = TablatureEditor.getInstance(context.getContext()).getTablature().getCurrentBeatRange();
+		if (beats!= null && !beats.isEmpty()) {
+			mode.setLoopSHeader(beats.firstMeasure().getNumber());
+			mode.setLoopEHeader(beats.lastMeasure().getNumber());
+		}
 		
 		MHeaderComboController mHeaderController = new MHeaderComboController(this.loopSHeader, this.loopEHeader);
 		mHeaderController.updateLoopSHeader( mode.getLoopSHeader() );

commit eae6bb1d9285fe3a778fcaa402b9157332c36abc

Fix bug when opening transport mode dialog
When something is selected with click & drag, loop range in transport mode
dialog (F9) is correctly initialized with selection
However, even when nothing was selected the values of loop range were updated

Scenario to reproduce bug:
- select measures 4 to 8 with click & drag
- type F9, loop range is initialized from 4 to 8 (OK)
- click 'training mode' then 'OK'
- click somewhere in measure 5 (empty selection)
- type F9: loop range is set from 5 to 5, even before clicking "OK" -> BUG

bug was introduced with 531495044d70a7e93afb29049527325cfbb7d8c4

TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/transport/TGTransportModeDialog.java
@@ -60,6 +60,7 @@ public class TGTransportModeDialog {
 		final UITableLayout dialogLayout = new UITableLayout();
 		final UIWindow dialog = uiFactory.createWindow(uiParent, true, false);
 		final TGBeatRange beats = this.context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_BEAT_RANGE);
+		boolean isSelectionActive = Boolean.TRUE.equals(this.context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_SELECTION_IS_ACTIVE));
 		
 		dialog.setLayout(dialogLayout);
 		dialog.setText(TuxGuitar.getProperty("transport.mode"));
@@ -202,7 +203,7 @@ public class TGTransportModeDialog {
 		rangeLayout.set(this.loopEHeader, 2, 2, UITableLayout.ALIGN_FILL, UITableLayout.ALIGN_FILL, true, true);
 		mHeaderRangeStatus.addControl(this.loopEHeader);
 		
-		if (beats!= null && !beats.isEmpty()) {
+		if (isSelectionActive && beats!= null && !beats.isEmpty()) {
 			mode.setLoopSHeader(beats.firstMeasure().getNumber());
 			mode.setLoopEHeader(beats.lastMeasure().getNumber());
 		}