32bd191f60e1b93dd24a4926512fafa4dd0ed767
Makoha <73967125+Makoha@users.noreply.github.com>
2024-04-08 21:56:52 +0200

    Improve tuplet handling (#331)
    Improvements related to issue #118
    
    This should allow tuxguitar to handle tuplets a lot better.
    Please note that because the value of TGDuration.QUARTER_TIME is 960, tuxguitar is not able to compute precise durations for 7, 9, 11 and 13 tuplets. Idealy TGDuration.QUARTER_TIME should be set to 2882880 in order for tuxguitar to compute precise durations for all these tuplets, but simply changing TGDuration.QUARTER_TIME currently has some unintended consequences that would need to be handled first.
    Because of this precision lose I have also added some patches that would fix some basic rounding errors when TGDuration.QUARTER_TIME is 960.

--- common/TuxGuitar-lib/src/org/herac/tuxguitar/song/models/TGDuration.java
+++ common/TuxGuitar-lib/src/org/herac/tuxguitar/song/models/TGDuration.java
@@ -6,6 +6,9 @@
  */
 package org.herac.tuxguitar.song.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.herac.tuxguitar.song.factory.TGFactory;
 /**
  * @author julian
@@ -15,7 +18,10 @@ import org.herac.tuxguitar.song.factory.TGFactory;
  */
 public abstract class TGDuration {
 	/**
-	 * tiempo por defecto de la Negra.
+	 * [ES] tiempo por defecto de la Negra.<br>
+	 * [EN] The number of fractions a quarter note can be divided into.
+	 *      This value should be equal to the Lowest Common Denominator (LCD)
+	 *      of all note durations including all supported tuplets.
 	 */
 	public static final long QUARTER_TIME = 960;
 	/**
@@ -52,13 +58,15 @@ public abstract class TGDuration {
 	 * Semi-Fusa.
 	 */
 	public static final int SIXTY_FOURTH = 64;
+	
+	/**
+	 * The shortest possible note (highest value)
+	 */
+	public static final int SHORTEST = SIXTY_FOURTH;
+	
 	/**
 	 * Valor.
 	 */
-	
-	// the shortest possible note (highest value)
-	public static final int SHORTEST = SIXTY_FOURTH;
-	
 	private int value;
 	/**
 	 * Puntillo.
@@ -72,6 +80,17 @@ public abstract class TGDuration {
 	 * DivisionType.
 	 */
 	private TGDivisionType divisionType;
+	
+	/**
+	 * Map containing all note durations that are
+	 * expected to be supported.<br>
+	 * Key = duration time
+	 */
+	private static final Map<Long, TGDuration> durationMap;
+	
+	static {
+		durationMap = createDurationMap();
+	}
 	
 	public TGDuration(TGFactory factory){
 		this.value = QUARTER;
@@ -118,49 +137,80 @@ public abstract class TGDuration {
 		return this.divisionType.convertTime(time);
 	}
 	
-	public static TGDuration fromTime(TGFactory factory,long time){
-		TGDuration duration = factory.newDuration();
-		duration.setValue(TGDuration.SIXTY_FOURTH);
-		duration.setDotted(false);
-		duration.setDoubleDotted(false);
-		duration.getDivision().setEnters(3);
-		duration.getDivision().setTimes(2);
-		return fromTime(factory,time,duration);
+	public static TGDuration getShortestDuration(TGFactory factory){
+		TGDuration minimum = factory.newDuration();
+		minimum.setValue(TGDuration.SHORTEST);
+		minimum.setDotted(false);
+		minimum.setDoubleDotted(false);
+		minimum.getDivision().setEnters(7);
+		minimum.getDivision().setTimes(4);
+		return minimum;
 	}
 	
-	public static TGDuration fromTime(TGFactory factory,long time,TGDuration minDuration){
-		return fromTime(factory, time, minDuration, 10);
+	public static TGDuration fromTime(TGFactory factory, long time){
+		TGDuration duration = getShortestDuration(factory);
+		return fromTime(factory, time, duration);
 	}
 	
-	public static TGDuration fromTime(TGFactory factory,long time,TGDuration minimum, int diff){
-		TGDuration duration = minimum.clone(factory);
-		TGDuration tmpDuration = factory.newDuration();
-		tmpDuration.setValue(TGDuration.WHOLE);
-		tmpDuration.setDotted(true);
-		boolean finish = false;
-		while(!finish){
-			long tmpTime = tmpDuration.getTime();
-			if(tmpTime - diff <= time){
-				if( Math.abs( tmpTime - time ) < Math.abs( duration.getTime() - time ) ){
-					duration = tmpDuration.clone(factory);
-				}
+	public static TGDuration fromTime(TGFactory factory, long time, TGDuration minDuration){
+		// The value of `threshold` is dependent on the value of
+		// `TGDuration.QUARTER_TIME` and the tuplets that are expected to be supported.
+		short threshold = 0;
+		
+		long quaterTimeConstant = QUARTER_TIME; // Local variable to suppress warnings
+		if(quaterTimeConstant == 960) {
+			// Patches for when QUARTER_TIME = 960
+			if(time < 60) {
+				// Notes shorter then the normal 64th note
+				threshold = 2;
 			}
-			if(tmpDuration.isDotted()){
-				tmpDuration.setDotted(false);
-			}else if(tmpDuration.getDivision().isEqual(TGDivisionType.NORMAL)){
-				tmpDuration.getDivision().setEnters(3);
-				tmpDuration.getDivision().setTimes(2);
-			}else{
-				tmpDuration.setValue(tmpDuration.getValue() * 2);
-				tmpDuration.setDotted(true);
-				tmpDuration.getDivision().setEnters(1);
-				tmpDuration.getDivision().setTimes(1);
+			else if(time == 95) {
+				// Fix going from any 64th tuplet note to a 64th 9-tuplet note
+				threshold = 3;
 			}
-			if(tmpDuration.getValue() > TGDuration.SIXTY_FOURTH){
-				finish = true;
+			else if(time == 138) {
+				// Fix going from 8th 7-tuplet note to 16th 7-tuplet note in a 4/16 measure
+				// also fixes going from 8th 7-tuplet note to 16th 7-tuplet note in a 4/8 measure | if(time == 412 || time == 549)
+				// also fixes going from 32th 7-tuplet note to 16th 7-tuplet note | if(time > 822 && time < 2882)
+				threshold = 1;
+			}
+			else if(time == 177) {
+				// Fix going from 64th 11-tuplet note to 32th 11-tuplet note
+				threshold = 3;
+			}
+			else if(time == 748) {
+				// Fix going from 16th 9-tuplet note to 32th 9-tuplet note
+				threshold = 2;
+			}
+			else if(time == 854) {
+				// Fix going from 64th 9-tuplet note to 32th 9-tuplet note
+				threshold = 1;
 			}
 		}
-		return duration;
+		return fromTime(factory, time, minDuration, threshold);
+	}
+	
+	public static TGDuration fromTime(TGFactory factory, long time, TGDuration minimum, short threshold) {
+		TGDuration durationFound = null;
+		for (int i = 0; i <= threshold; i++)
+		{
+			durationFound = durationMap.get(time-i);
+			if(durationFound != null) {
+				return durationFound.clone(factory);
+			}
+		}
+		
+		durationFound = factory.newDuration();
+		
+		for (int v = TGDuration.WHOLE; v <= SHORTEST; v *= 2)
+		{
+			durationFound.setValue(v);
+			if(time >= durationFound.getTime()) {
+				return durationFound.clone(factory);	
+			}
+		}
+		
+		return minimum.clone(factory);
 	}
 	
 	public int getIndex(){
@@ -187,5 +237,53 @@ public abstract class TGDuration {
 		this.setDotted(duration.isDotted());
 		this.setDoubleDotted(duration.isDoubleDotted());
 		this.getDivision().copyFrom(duration.getDivision());
+	}
+	
+	private static Map<Long, TGDuration> createDurationMap() {
+		TGFactory factory = new TGFactory();
+		HashMap<Long, TGDuration> durationHashMap = new HashMap<Long, TGDuration>();
+		
+		// Create an array of all known DivisionTypes
+		TGDivisionType[] allDivisionTypes = new TGDivisionType[TGDivisionType.ALTERED_DIVISION_TYPES.length + 1];
+		allDivisionTypes[0] = TGDivisionType.NORMAL;
+		for (int i = 1; i < allDivisionTypes.length; i++) {
+			allDivisionTypes[i] = TGDivisionType.ALTERED_DIVISION_TYPES[i - 1];
+		}
+		
+		for(TGDivisionType tmpDivisionType : allDivisionTypes)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				for (int v = TGDuration.WHOLE; v <= SHORTEST; v *= 2)
+				{
+					TGDuration tmpDuration = factory.newDuration();
+					
+					if(i == 1) {
+						tmpDuration.setDotted(true);
+						tmpDuration.setDoubleDotted(false);
+					}
+					else if(i == 2) {
+						tmpDuration.setDotted(false);
+						tmpDuration.setDoubleDotted(true);
+					}
+					else {
+						tmpDuration.setDotted(false);
+						tmpDuration.setDoubleDotted(false);
+					}
+					
+					tmpDuration.setValue(v);
+					tmpDuration.getDivision().setEnters(tmpDivisionType.getEnters());
+					tmpDuration.getDivision().setTimes(tmpDivisionType.getTimes());
+					
+					TGDuration entry = tmpDuration;
+					long key = entry.getTime();
+					if(!durationHashMap.containsKey(key)) {
+						durationHashMap.put(key, entry);
+					}
+				}
+			}
+		}
+		
+		return durationHashMap;
 	}
 }
