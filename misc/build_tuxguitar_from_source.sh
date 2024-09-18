95573851874d8f46b7c9566c1f3026bed951f017
helge17 <5897631+helge17@users.noreply.github.com>
2024-09-18 22:44:08 +0200

    Make build script work locally under MacOS and FreeBSD (#291, #298) and make it more robust

--- misc/build_tuxguitar_from_source.sh
+++ misc/build_tuxguitar_from_source.sh
@@ -5,7 +5,11 @@
 # Print commands
 #set -x
 
-COMMAND=`basename $0`" $@"
+# Software needed to build TuxGuitar is located in this directory
+SW_DIR=~/Software/TuxGuitar/tuxguitar_build_dependencies
+
+# This script
+SCRIPT=`basename $0`
 SCRIPT_DIR=`dirname $0`
 SCRIPT_DIR=`cd "$SCRIPT_DIR"; pwd`
 
@@ -14,8 +18,17 @@ SRC_DIR=$SCRIPT_DIR/..
 
 cd $SRC_DIR
 
+# Binary packages are placed into this directory
+DIST_DIR=`pwd`/00-Binary_Packages
+
 # Current git branch
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+# Current source version
+TGSRCVER=`grep 'CURRENT = new TGVersion' common/TuxGuitar-lib/src/org/herac/tuxguitar/util/TGVersion.java | awk -F '[(,)]' '{ print $2"."$3"."$4 }'`
+
+# Default build version
+TGVERSION=`date +%Y`-`date +%m`-`date +%d`"-$GIT_BRANCH"
 
 function usage {
   echo
@@ -24,7 +37,8 @@ function usage {
   echo "# TuxGuitar build script for https://github.com/helge17/tuxguitar"
   echo "#"
   echo "# I use this script to build TuxGuitar for Linux, Windows and Android on Debian 12 (Bookworm)"
-  echo "# and on remote FreeBSD and MacOS systems."
+  echo "# and on FreeBSD and MacOS."
+  echo "#"
   echo "# The script heavily depends on my build environment, so examine it carefully and modify it to"
   echo "# your needs before starting it on your computer!"
   echo "# The script contains ugly hacks and modifies many source files, so only start it on a copy of"
@@ -32,20 +46,31 @@ function usage {
   echo "#"
   echo "# Usage: "`basename $0`" [OPTIONS]"
   echo "#"
-  echo "# -l     Build for Linux"
-  echo "# -w     Build for Windows"
-  echo "# -m     Build for MacOS"
-  echo "# -b     Build for FreeBSD"
-  echo "# -a     Build for Android"
-  echo "# -A     Build for all"
+  echo "# -l      Build for Linux"
+  echo "# -w      Build for Windows"
+  echo "# -a      Build for Android"
+  echo "# -m      Build for MacOS"
+  echo "# -b      Build for FreeBSD"
+  echo "# -A      Build for all OS"
+  echo "# -g, -G  Upload the builds to Github"
+  echo "# -r <r>  Set the build version to <r>"
+  echo "# -h      Show this help"
   echo "#"
-  echo "# -g, -G Create a new Github release and upload the builds."
+  echo "# -l, -w and -a build TuxGuitar for Linux/Windows/Android on the local Linux (Debian) system."
+  echo "#"
+  echo "# -m, -b: When started on Linux, the script connects to a remote MacOS/FreeBSD system to"
+  echo "#         build TuxGuitar. When started on MacOS/FreeBSD, TuxGuitar is built on the local"
+  echo "#         system."
+  echo "#"
+  echo "# -A: Same as -lwamb"
+  echo "#"
+  echo "# -g, -G create a new Github release and upload the builds (from Linux only)."
   echo "#        -g keeps the release in draft status after the upload. The draft won’t be seen by the"
   echo "#           public unless it is published manually, either in the Github web interface or with"
   echo "#           the command"
   echo "#           $ gh release edit <release> --draft=false"
-  echo "#           Then also the release tag will be created on Github."
-  echo "#           After that you also have to sync the website manually."
+  echo "#           Then, also the release tag will be created on Github."
+  echo "#           After that, you also have to sync the website manually."
   echo "#        -G publishes the release immediately and syncs the website folder to the webserver if"
   echo "#           all files were successfully uploaded. If one or more uploads failed, the release"
   echo "#           remains in draft status and the website is not synced (as with -g)."
@@ -67,14 +92,8 @@ function usage {
   echo
 }
 
-# Current source version
-TGSRCVER=`grep 'CURRENT = new TGVersion' common/TuxGuitar-lib/src/org/herac/tuxguitar/util/TGVersion.java | awk -F '[(,)]' '{ print $2"."$3"."$4 }'`
-
-# Default build version
-TGVERSION=`date +%Y`-`date +%m`-`date +%d`"-$GIT_BRANCH"
-
 # Parse command line options
-while getopts "lwmbaAgGhr:" CMDopt; do
+while getopts "lwambAgGhr:" CMDopt; do
   case $CMDopt in
     l) build_linux=1;;
     w) build_windows=1;;
@@ -104,26 +123,10 @@ while getopts "lwmbaAgGhr:" CMDopt; do
   esac
 done
 
-if [ "$#" -lt 1 ]; then
-  usage
+function abort_build {
+  echo -e "\n### Aborting build.\n"
   exit 1
-fi
-
-# Binary packages are placed into this directory
-DIST_DIR=`pwd`/00-Binary_Packages
-
-# Software needed to build TuxGuitar is located in this directory
-SW_DIR=~/Software/TuxGuitar/tuxguitar_build_dependencies
-
-mkdir -p $SW_DIR
-
-echo
-echo "### Host: "`hostname -s`" #################################################"
-echo "### Build version: $TGVERSION"
-echo "### Building TuxGuitar from source. All Builds will be placed into the dirctory"
-echo "### $DIST_DIR"
-echo "### Host: "`hostname -s`" #################################################"
-mkdir -p $DIST_DIR
+}
 
 function release_checks_before_prepare_source {
 
@@ -134,8 +137,7 @@ function release_checks_before_prepare_source {
     echo -e "OK.\n"
   else
     echo -e " no.\n\nHEAD branch is $GIT_BRANCH. Please switch local HEAD to master."
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   fi
 
   echo -n "# Checking newest entry in CHANGES file ... "
@@ -145,8 +147,7 @@ function release_checks_before_prepare_source {
     echo -e "# OK.\n"
   else
     echo -e "first line not matching \"TuxGuitar $TGVERSION (YYYY-MM-DD) changes:\"."
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   fi
 
   echo -n "# Checking download and help links on website ... "
@@ -156,16 +157,14 @@ function release_checks_before_prepare_source {
     echo -e "# OK.\n"
   else
     echo -e "not found."
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   fi
 
   echo -n "# Checking for local source changes ... "
   if [ -n "$(git status --ignored=traditional --porcelain)" ]; then
     echo -e "found:\n"
     git status --ignored=traditional
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   else
     echo -e "none found, OK.\n"
   fi
@@ -174,8 +173,7 @@ function release_checks_before_prepare_source {
   if [ -n "$(git log origin/master..HEAD)" ]; then
     echo -e "found:\n"
     git log origin/master..HEAD
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   else
     echo -e "none found, OK.\n"
   fi
@@ -188,8 +186,7 @@ function release_checks_before_prepare_source {
   if [ -n "$(git log HEAD..origin/master)" ]; then
     echo -e "found:\n"
     git log HEAD..origin/master
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   else
     echo -e "none found, OK.\n"
   fi
@@ -200,8 +197,7 @@ function release_checks_before_prepare_source {
     echo -e "Android translations have changed:\n"
     git status --ignored=traditional
     echo -e "\nPlease commit the new translations."
-    echo -e "\n### Aborting build.\n"
-    exit 1
+    abort_build
   else
     echo -e "Android translations are up to date, OK.\n"
   fi
@@ -220,17 +216,16 @@ if [ -e .build-version ]; then
   else
     echo -e "\n# Build version was already set to $OLDVERSION in a previous run of `basename $0`."
     echo "# Please confirm that you want to build version $OLDVERSION with the option '-r $OLDVERSION'."
-    echo -e "\n### Host: "`hostname -s`" ########### Aborting build.\n"
-    exit 1
+    abort_build
   fi
 fi
 
-echo -e "\n### Host: "`hostname -s`" ########### Hacks ...\n"
+echo -e "\n### Host: "`hostname -s`" ########### Hacks ..."
 
 echo -e "\n# Change build version from 9.99-SNAPSHOT to $TGVERSION in config files ..."
-  find . \( -name "*.xml" -or -name "*.gradle"  -or -name "*.properties" -or -name "*.html" -or -name control -or -name Info.plist -or -name CHANGES \) -and -not -path "./website/*" -and -type f -exec sed -i "s/9.99-SNAPSHOT/$TGVERSION/" '{}' \;
+  find . \( -name "*.xml" -or -name "*.gradle"  -or -name "*.properties" -or -name "*.html" -or -name control -or -name Info.plist -or -name CHANGES \) -and -not -path "./website/*" -and -type f -exec sed -i -e "s/9.99-SNAPSHOT/$TGVERSION/" '{}' \;
   # Also set the version in the "Help - About" dialog
-  sed -i "s/static final String RELEASE_NAME =.*/static final String RELEASE_NAME = (TGApplication.NAME + \" $TGVERSION\");/" desktop/TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/about/TGAboutDialog.java
+  sed -i -e "s/static final String RELEASE_NAME =.*/static final String RELEASE_NAME = (TGApplication.NAME + \" $TGVERSION\");/" desktop/TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/about/TGAboutDialog.java
 echo "# OK."
 
 echo $TGVERSION > .build-version
@@ -248,7 +243,7 @@ function release_checks_after_prepare_source {
   else
     echo -e "found:\n"
     diff -r desktop/TuxGuitar/share/help/ website/files/$TGVERSION/desktop/help/
-    echo -e "\n### Aborting build.\n"
+    abort_build
   fi
 
 }
@@ -404,7 +399,7 @@ rm -rf desktop/build-scripts/common-resources/common-windows/jre
 cp -ai $SW_DIR/$PA_JAVA desktop/build-scripts/common-resources/common-windows/jre
 
 for GUI_TK in swt jfx; do
-  echo -e "\n### Host: "`hostname -s`" ########### Building Windows $GUI_TK $BUILD_ARCH ZIP & INSTALL (including Java) ..."
+  echo -e "\n### Host: "`hostname -s`" ########### Building Windows $GUI_TK $BUILD_ARCH ZIP & INSTALL (including Java) ...\n"
 
   cd desktop/build-scripts/tuxguitar-windows-$GUI_TK-$BUILD_ARCH-installer
   # As we are building the Windows version on Linux, we explicitly deactivate the Linux profile and select the Windows profile manually to avoid confusion.
@@ -437,7 +432,7 @@ rsync --verbose --archive --delete --exclude=00-Binary_Packages/* --delete-exclu
 echo "# OK."
 echo -e "\n### Host: "`hostname -s`" ########### Preparing the build for BSD TAR.GZ done."
 
-ssh $BUILD_HOST "cd $SRC_PATH && misc/$COMMAND"
+ssh $BUILD_HOST "cd $SRC_PATH && misc/$SCRIPT -b -r $TGVERSION"
 scp -p $BUILD_HOST:$SRC_PATH/00-Binary_Packages/tuxguitar-$TGVERSION-freebsd-*-*.tar.gz $DIST_DIR
 
 }
@@ -450,7 +445,7 @@ install_eclipse_swt
 install_openjfx
 
 for GUI_TK in swt jfx; do
-  echo -e "\n### Host: "`hostname -s`" ########### Building BSD $GUI_TK $BUILD_ARCH TAR.GZ ..."
+  echo -e "\n### Host: "`hostname -s`" ########### Building BSD $GUI_TK $BUILD_ARCH TAR.GZ ...\n"
 
   cd desktop/build-scripts/tuxguitar-freebsd-$GUI_TK
   mvn --batch-mode -e clean verify -P native-modules
@@ -483,7 +478,7 @@ rsync --verbose --archive --delete --exclude=00-Binary_Packages/* --delete-exclu
 echo "# OK."
 echo -e "\n### Host: "`hostname -s`" ########### Preparing the build for MacOS APP done."
 
-ssh $BUILD_HOST "cd $SRC_PATH && misc/$COMMAND"
+ssh $BUILD_HOST "cd $SRC_PATH && misc/$SCRIPT -m -r $TGVERSION"
 # On my MacOS 14 VM, the outgoing transfer rate via scp is terribly slow.
 # Without -X options:                 ~ 15KB/s  (always!!!)
 # With -X nrequests=1 -X buffer=2048: ~ 250KB/s (bridged -> WLAN -> Linux system), 5MB/s (NAT -> local VMware host)
@@ -502,7 +497,7 @@ BUILD_ARCH=`uname -m`
 install_eclipse_swt
 
 for GUI_TK in swt jfx; do
-  echo -e "\n### Host: "`hostname -s`" ########### Building MacOS $GUI_TK $BUILD_ARCH APP ..."
+  echo -e "\n### Host: "`hostname -s`" ########### Building MacOS $GUI_TK $BUILD_ARCH APP ...\n"
 
   cd desktop/build-scripts/tuxguitar-macosx-$GUI_TK-cocoa
   mvn --batch-mode -e clean verify
@@ -632,16 +627,33 @@ function copy_to_github {
 
 }
 
-# Check and prepare source code. This is done on Linux for all platforms.
-if [ `uname` == Linux ]; then
-  [ $TGVERSION == $TGSRCVER ] && release_checks_before_prepare_source
-  prepare_source
-  [ $TGVERSION == $TGSRCVER ] && release_checks_after_prepare_source
-fi
+# Validate command line options
+[ "$#" -lt 1 ] && usage && exit 1
+[ $build_linux ]    && [ `uname` != Linux ]   && echo -e "\nError: Linux version can only be built on Linux."            && abort_build
+[ $build_windows ]  && [ `uname` != Linux ]   && echo -e "\nError: Windows version can only be built on Linux."          && abort_build
+[ $build_macos ]    && [ `uname` == FreeBSD ] && echo -e "\nError: MacOS version cannnot be built on/from FreeBSD."      && abort_build
+[ $build_bsd ]      && [ `uname` == Darwin ]  && echo -e "\nError: FreeBSD version cannnot be built on/from MacOS."      && abort_build
+[ $build_android ]  && [ `uname` != Linux ]   && echo -e "\nError: Android version can only be built on Linux."          && abort_build
+[ $copy_to_github ] && [ `uname` != Linux ]   && echo -e "\nError: A new Github release can only be created from Linux." && abort_build
+
+mkdir -p $SW_DIR
+
+echo
+echo "### Host: "`hostname -s`" #################################################"
+echo "### Build version: $TGVERSION"
+echo "### Building TuxGuitar from source. All Builds will be placed into the dirctory"
+echo "### $DIST_DIR"
+echo "### Host: "`hostname -s`" #################################################"
+mkdir -p $DIST_DIR
+
+# Check and prepare source code
+[ $TGVERSION == $TGSRCVER ] && release_checks_before_prepare_source
+prepare_source
+[ $TGVERSION == $TGSRCVER ] && release_checks_after_prepare_source
 
 # First, we start the remote builds to avoid copying all locally created binaries to the remote hosts.
 
-# BSD
+# BSD (on BSD, remote or local)
 if [ $build_bsd ]; then
   # SWT & JFX versions in FreeBSD 13.2
   SWT_VERSION=4.21
@@ -651,7 +663,7 @@ if [ $build_bsd ]; then
   [ `uname` == FreeBSD ] && build_tg_for_bsd
 fi
 
-# MacOS
+# MacOS (on MacOS, remote or local)
 if [ $build_macos ]; then
   SWT_VERSION=4.14
   SWT_DATE=201912100610
@@ -662,7 +674,7 @@ fi
 
 # Then come the local builds.
 
-# Linux
+# Linux (local on Linux)
 if [ $build_linux ]; then
   # SWT version in Debian 12
   SWT_VERSION=4.26
@@ -671,7 +683,7 @@ if [ $build_linux ]; then
   [ `uname` == Linux ] && build_tg_for_linux
 fi
 
-# Windows 64 bit x86_64 build
+# Windows 64 bit x86_64 build (local on Linux)
 if [ $build_windows ]; then
   SWT_VERSION=4.26
   SWT_DATE=202211231800
@@ -682,12 +694,8 @@ if [ $build_windows ]; then
   [ `uname` == Linux ] && build_tg_for_windows
 fi
 
-# Android build
-if [ $build_android ]; then
-  [ `uname` == Linux ] && build_tg_for_android
-fi
+# Android build (local on Linux)
+[ `uname` == Linux ] && [ $build_android ] && build_tg_for_android
 
-# Create a new Github release and upload the builds
-if [ $copy_to_github ]; then
-  [ `uname` == Linux ] && copy_to_github
-fi
+# Create a new Github release and upload the builds (Linux only)
+[ `uname` == Linux ] && [ $copy_to_github ] && copy_to_github
