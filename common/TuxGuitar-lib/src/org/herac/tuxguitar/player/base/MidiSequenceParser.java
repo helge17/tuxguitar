helge17/tuxguitar

commit 2b1111ee4267ca9132e36c873eef0ba88d51019c

midi and audio export: only audible tracks

common/TuxGuitar-lib/src/org/herac/tuxguitar/player/base/MidiSequenceParser.java
@@ -112,8 +112,16 @@ public class MidiSequenceParser {
 		
 		this.addDefaultMessages(helper, this.song);
 		
+		boolean existsSoloTrack = false;
 		for (int i = 0; i < this.song.countTracks(); i++) {
-			addTrack(helper, this.song.getTrack(i));
+			existsSoloTrack |= this.song.getTrack(i).isSolo();
+		}
+		
+		for (int i = 0; i < this.song.countTracks(); i++) {
+			TGTrack track = this.song.getTrack(i);
+			if (!track.isMute() && (!existsSoloTrack || track.isSolo())) {
+				addTrack(helper, track);
+			}
 		}
 		sequence.notifyFinish();
 	}

commit 5848f86c500e980d9e2dfe54730d31bb012cece2

fix several issues when first track is muted
bug introduced by 2b1111ee4267ca9132e36c873eef0ba88d51019c
many possible consequences when first track is muted:
erroneous tempo, no metronome,...

common/TuxGuitar-lib/src/org/herac/tuxguitar/player/base/MidiSequenceParser.java
@@ -116,12 +116,9 @@ public class MidiSequenceParser {
 		for (int i = 0; i < this.song.countTracks(); i++) {
 			existsSoloTrack |= this.song.getTrack(i).isSolo();
 		}
-		
 		for (int i = 0; i < this.song.countTracks(); i++) {
 			TGTrack track = this.song.getTrack(i);
-			if (!track.isMute() && (!existsSoloTrack || track.isSolo())) {
-				addTrack(helper, track);
-			}
+			addTrack(helper, track, !track.isMute() && (!existsSoloTrack || track.isSolo()));
 		}
 		sequence.notifyFinish();
 	}
@@ -139,7 +136,7 @@ public class MidiSequenceParser {
 		}
 	}
 	
-	private void addTrack(MidiSequenceHelper sh, TGTrack track) {
+	private void addTrack(MidiSequenceHelper sh, TGTrack track, boolean shallPlay) {
 		TGChannel tgChannel = this.songManager.getChannel(this.song, track.getChannelId() );
 		if( tgChannel != null ){
 			TGMeasure previous = null;
@@ -157,9 +154,10 @@ public class MidiSequenceParser {
 					addTempo(sh,measure, previous, mh.getMove());
 					addMetronome(sh,measure.getHeader(), mh.getMove() );
 				}
-				//agrego los pulsos
-				addBeats( sh, tgChannel, track, measure, mIndex, mh.getMove() );
-				
+				if (shallPlay) {
+					//agrego los pulsos
+					addBeats( sh, tgChannel, track, measure, mIndex, mh.getMove() );
+				}
 				previous = measure;
 			}
 		}