91d5eeb9ca2cb2bf3f41dd035bd944722fad7812
helge17 <5897631+helge17@users.noreply.github.com>
2025-05-17 16:49:13 +0200

    Rename Android app back to TuxGuitar (#138, #41)

--- android/TuxGuitar-android/src/app/tuxguitar/android/variables/TGVarAppName.java
+++ android/TuxGuitar-android/src/app/tuxguitar/android/variables/TGVarAppName.java
@@ -3,7 +3,7 @@ package app.tuxguitar.android.variables;
 public class TGVarAppName {
 
 	public static final String NAME = "appname";
-	public static final String APPLICATION_NAME = "TuxGuitar+";
+	public static final String APPLICATION_NAME = "TuxGuitar";
 
 	public String toString() {
 		return APPLICATION_NAME;
