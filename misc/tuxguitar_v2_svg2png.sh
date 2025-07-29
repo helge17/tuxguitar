5ba3c359f49b8e424adadf7cc46ebe5340a930e7
helge17 <5897631+helge17@users.noreply.github.com>
2025-07-29 02:27:36 +0200

    Update macOS icon

--- misc/tuxguitar_v2_svg2png.sh
+++ misc/tuxguitar_v2_svg2png.sh
@@ -33,13 +33,31 @@ inkscape $LOGO   --export-filename=$TG_DIR/android/TuxGuitar-android/res/drawabl
 inkscape $LOGO   --export-filename=$TG_DIR/android/TuxGuitar-android/res/drawable-xhdpi/ic_launcher.png                                   --export-background-opacity=0 --export-width=96  --export-height=96
 inkscape $LOGO   --export-filename=$TG_DIR/android/TuxGuitar-android/res/drawable-xxhdpi/ic_launcher.png                                  --export-background-opacity=0 --export-width=144 --export-height=144
 
+TMPDIR=$(mktemp --directory --suffix -macos)
+inkscape $LOGO   --export-filename=$TMPDIR/icon_320x320.png                                                                               --export-background-opacity=0 --export-width=320  --export-height=320
+  magick -size 512x512 xc:transparent -fill 'gradient:#536586-#154374' -draw "roundrectangle 50,50 462,462 95,95" $TMPDIR/frame_512x512.png
+  gm composite -gravity Center $TMPDIR/icon_320x320.png $TMPDIR/frame_512x512.png $TMPDIR/icns_512x512.png
+    gm convert -scale 256x256 $TMPDIR/icns_512x512.png $TMPDIR/icns_256x256.png
+    gm convert -scale 128x128 $TMPDIR/icns_512x512.png $TMPDIR/icns_128x128.png
+    gm convert -scale 32x32   $TMPDIR/icns_512x512.png $TMPDIR/icns_32x32.png
+    gm convert -scale 16x16   $TMPDIR/icns_512x512.png $TMPDIR/icns_16x16.png
+      png2icns $TG_DIR/desktop/build-scripts/common-resources/common-macosx/Contents/Resources/icon.icns $TMPDIR/icns_*.png
+inkscape $LOGO   --export-filename=$TMPDIR/icon_412x412.png                                                                               --export-background-opacity=0 --export-width=412 --export-height=412
+  gm convert -border 50x50 -bordercolor transparent $TMPDIR/icon_412x412.png $TMPDIR/tgdoc_512x512.png
+    gm convert -scale 256x256 $TMPDIR/tgdoc_512x512.png $TMPDIR/tgdoc_256x256.png
+    gm convert -scale 128x128 $TMPDIR/tgdoc_512x512.png $TMPDIR/tgdoc_128x128.png
+    gm convert -scale 32x32   $TMPDIR/tgdoc_512x512.png $TMPDIR/tgdoc_32x32.png
+    gm convert -scale 16x16   $TMPDIR/tgdoc_512x512.png $TMPDIR/tgdoc_16x16.png
+      png2icns $TG_DIR/desktop/build-scripts/common-resources/common-macosx/Contents/Resources/tgdoc.icns $TMPDIR/tgdoc_*.png
+rm -rf $TMPDIR
+
 # Splash screen
 
 inkscape $SPLASH --export-filename=$TG_DIR/desktop/TuxGuitar/src-skins/Breeze/TuxGuitar/splash.png                                        --export-background-opacity=1
   cp -p                            $TG_DIR/desktop/TuxGuitar/src-skins/Breeze/TuxGuitar/splash.png $TG_DIR/desktop/build-scripts/common-resources/common-windows-installer-icons/TG_splash_500x300.png
 
-inkscape $SPLASH --export-filename=desktop/build-scripts/common-resources/common-windows-installer-icons/TG_logo_163x314.png --export-type=png --export-background-opacity=1 --export-height=314
-gm mogrify -shave 55x0 -crop 163x314 desktop/build-scripts/common-resources/common-windows-installer-icons/TG_logo_163x314.png
+inkscape $SPLASH --export-filename=$TG_DIR/desktop/build-scripts/common-resources/common-windows-installer-icons/TG_logo_163x314.png --export-type=png --export-background-opacity=1 --export-height=314
+gm mogrify -shave 55x0 -crop 163x314 $TG_DIR/desktop/build-scripts/common-resources/common-windows-installer-icons/TG_logo_163x314.png
 
 # Normally done with build_skin.sh
 for skin in Symbolic Symbolic-dark Breeze Breeze-dark Oxygen Oxygen-dark; do
