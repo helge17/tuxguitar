helge17/tuxguitar

commit 3669dca52c7fd51472a3f2742f4be16a5738f9b1

player mode dialog, simplify entry with keyboard
Scenario:
- open player mode dialog (F9)
- activate "Training mode" option
- select "From" and type "50"
- type "tab" key (it selects "To"), and try to type "60"

There is no timer to distinguish between "60" and "6 ... then 0"
Before this PR, the "6" was ignored because 6<50, so it's an invalid value

This PR applies another approach: all user inputs are accepted
but if From/To are inconsistent, OK button is disabled and last edited field
is shown with red background

desktop/TuxGuitar/src/app/tuxguitar/app/view/dialog/transport/TGTransportModeDialog.java
@@ -8,6 +8,8 @@ import app.tuxguitar.app.action.impl.caret.TGMoveToAction;
 import app.tuxguitar.app.action.impl.transport.TGTransportModeAction;
 import app.tuxguitar.app.document.TGDocument;
 import app.tuxguitar.app.document.TGDocumentListManager;
+import app.tuxguitar.app.system.config.TGConfigKeys;
+import app.tuxguitar.app.system.config.TGConfigManager;
 import app.tuxguitar.app.ui.TGApplication;
 import app.tuxguitar.app.view.controller.TGViewContext;
 import app.tuxguitar.app.view.util.TGDialogUtil;
@@ -20,9 +22,13 @@ import app.tuxguitar.song.models.TGMeasureHeader;
 import app.tuxguitar.song.models.TGSong;
 import app.tuxguitar.song.models.TGTrack;
 import app.tuxguitar.ui.UIFactory;
+import app.tuxguitar.ui.event.UICloseEvent;
+import app.tuxguitar.ui.event.UICloseListener;
 import app.tuxguitar.ui.event.UISelectionEvent;
 import app.tuxguitar.ui.event.UISelectionListener;
 import app.tuxguitar.ui.layout.UITableLayout;
+import app.tuxguitar.ui.resource.UIColor;
+import app.tuxguitar.ui.resource.UIColorModel;
 import app.tuxguitar.ui.widget.UIButton;
 import app.tuxguitar.ui.widget.UICheckBox;
 import app.tuxguitar.ui.widget.UIControl;
@@ -50,12 +56,18 @@ public class TGTransportModeDialog {
 	protected UISpinner customFrom;
 	protected UISpinner customTo;
 	protected UISpinner customIncrement;
+	
+	protected UIButton buttonOK;
+	protected UIColorModel colorModelErr;
+	protected UIColor colorErr;
+	protected UIColor colorOK;
 
 	protected UIDropDownSelect<Integer> loopSHeader;
 	protected UIDropDownSelect<Integer> loopEHeader;
 
 	public TGTransportModeDialog(TGViewContext context){
 		this.context = context;
+		this.colorModelErr = TGConfigManager.getInstance(context.getContext()).getColorModelConfigValue(TGConfigKeys.COLOR_INVALID_ENTRY);
 	}
 
 	public void show(){
@@ -66,9 +78,18 @@ public class TGTransportModeDialog {
 		final UIWindow dialog = uiFactory.createWindow(uiParent, true, false);
 		final TGBeatRange beats = this.context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_BEAT_RANGE);
 		boolean isSelectionActive = Boolean.TRUE.equals(this.context.getAttribute(TGDocumentContextAttributes.ATTRIBUTE_SELECTION_IS_ACTIVE));
+		this.colorErr = uiFactory.createColor(this.colorModelErr);
 
 		dialog.setLayout(dialogLayout);
 		dialog.setText(TuxGuitar.getProperty("transport.mode"));
+		dialog.addCloseListener(new UICloseListener() {
+			@Override
+			public void onClose(UICloseEvent event) {
+				if ((TGTransportModeDialog.this.colorErr != null) && !TGTransportModeDialog.this.colorErr.isDisposed()) {
+					TGTransportModeDialog.this.colorErr.dispose();
+				}
+			}
+		});
 
 		// ----------------------------------------------------------------------
 
@@ -228,16 +249,16 @@ public class TGTransportModeDialog {
 		buttons.setLayout(buttonsLayout);
 		dialogLayout.set(buttons, 6, 1, UITableLayout.ALIGN_RIGHT, UITableLayout.ALIGN_FILL, true, true);
 
-		UIButton buttonOK = uiFactory.createButton(buttons);
-		buttonOK.setText(TuxGuitar.getProperty("ok"));
-		buttonOK.setDefaultButton();
-		buttonOK.addSelectionListener(new UISelectionListener() {
+		this.buttonOK = uiFactory.createButton(buttons);
+		this.buttonOK.setText(TuxGuitar.getProperty("ok"));
+		this.buttonOK.setDefaultButton();
+		this.buttonOK.addSelectionListener(new UISelectionListener() {
 			public void onSelect(UISelectionEvent event) {
 				changeTransportMode();
 				dialog.dispose();
 			}
 		});
-		buttonsLayout.set(buttonOK, 1, 1, UITableLayout.ALIGN_FILL, UITableLayout.ALIGN_FILL, true, true, 1, 1, 80f, 25f, null);
+		buttonsLayout.set(this.buttonOK, 1, 1, UITableLayout.ALIGN_FILL, UITableLayout.ALIGN_FILL, true, true, 1, 1, 80f, 25f, null);
 
 		UIButton buttonCancel = uiFactory.createButton(buttons);
 		buttonCancel.setText(TuxGuitar.getProperty("cancel"));
@@ -249,9 +270,10 @@ public class TGTransportModeDialog {
 		buttonsLayout.set(buttonCancel, 1, 2, UITableLayout.ALIGN_FILL, UITableLayout.ALIGN_FILL, true, true, 1, 1, 80f, 25f, null);
 		buttonsLayout.set(buttonCancel, UITableLayout.MARGIN_RIGHT, 0f);
 
+		this.colorOK = this.customFrom.getBgColor();
 		TGDialogUtil.openDialog(dialog,TGDialogUtil.OPEN_STYLE_CENTER | TGDialogUtil.OPEN_STYLE_PACK);
 	}
-
+	
 	public void changeTransportMode() {
 		Integer type = (this.custom.isSelected() ? MidiPlayerMode.TYPE_CUSTOM : MidiPlayerMode.TYPE_SIMPLE );
 		Boolean loop = (type == MidiPlayerMode.TYPE_CUSTOM || (type == MidiPlayerMode.TYPE_SIMPLE && this.simpleLoop.isSelected()));
@@ -331,17 +353,29 @@ public class TGTransportModeDialog {
 				if( this.from.getValue() < MIN_SELECTION){
 					this.from.setValue(MIN_SELECTION);
 				}else if(this.from.getValue() >= this.to.getValue()){
-					this.from.setValue(this.to.getValue() - 1);
+					// invalid
+					TGTransportModeDialog.this.buttonOK.setEnabled(false);
+					this.from.setBgColor(TGTransportModeDialog.this.colorErr);
+					this.to.setBgColor(TGTransportModeDialog.this.colorOK);
 				}
 			}else if(event.getComponent().equals(this.to)){
 				if( this.to.getValue() <= this.from.getValue()){
-					this.to.setValue(this.from.getValue() + 1);
+					// invalid
+					TGTransportModeDialog.this.buttonOK.setEnabled(false);
+					this.to.setBgColor(TGTransportModeDialog.this.colorErr);
+					this.from.setBgColor(TGTransportModeDialog.this.colorOK);
 				}else if(this.to.getValue() > MAX_SELECTION){
 					this.to.setValue(MAX_SELECTION);
 				}
 			}
-			if( this.increment.getValue() > (this.to.getValue() - this.from.getValue())){
-				this.increment.setValue(this.to.getValue() - this.from.getValue());
+			if( this.to.getValue() > this.from.getValue()){
+				// valid
+				this.from.setBgColor(TGTransportModeDialog.this.colorOK);
+				this.to.setBgColor(TGTransportModeDialog.this.colorOK);
+				TGTransportModeDialog.this.buttonOK.setEnabled(true);
+				if( this.increment.getValue() > (this.to.getValue() - this.from.getValue())){
+					this.increment.setValue(this.to.getValue() - this.from.getValue());
+				}
 			}
 		}
 	}

desktop/TuxGuitar/src/app/tuxguitar/app/system/config/TGConfigDefaults.java
@@ -84,6 +84,7 @@ public class TGConfigDefaults{
 		loadProperty(properties, TGConfigKeys.COLOR_LOOP_S_MARKER, "42,165,42");
 		loadProperty(properties, TGConfigKeys.COLOR_LOOP_E_MARKER, "165,42,42");
 		loadProperty(properties, TGConfigKeys.COLOR_MEASURE_NUMBER, "128,0,0");
+		loadProperty(properties, TGConfigKeys.COLOR_INVALID_ENTRY, "230,110,110");
 		loadProperty(properties, TGConfigKeys.MIDI_PORT, "tuxguitar-synth.port");
 		loadProperty(properties, TGConfigKeys.MIDI_SEQUENCER, "tuxguitar.sequencer");
 		loadProperty(properties, TGConfigKeys.MAX_HISTORY_FILES, 10);

desktop/TuxGuitar/src/app/tuxguitar/app/system/config/TGConfigKeys.java
@@ -57,6 +57,7 @@ public class TGConfigKeys {
 	public static final String COLOR_LOOP_S_MARKER = "color.loop.s.marker";
 	public static final String COLOR_LOOP_E_MARKER = "color.loop.e.marker";
 	public static final String COLOR_MEASURE_NUMBER = "color.measure.number";
+	public static final String COLOR_INVALID_ENTRY = "color.invalid.entry";
 	public static final String MIDI_PORT = "midi.port";
 	public static final String MIDI_SEQUENCER = "midi.sequencer";
 	public static final String MAX_HISTORY_FILES = "history.max-files";

commit a302f3fc5f6b7761cdedaa06d90d2a3a7f09d4d6

fix bug when closing transport mode dialog
bug: open transport mode dialog (F9) and try to close window
(without using Cancel/OK), it doesn't work

bug introduced by 3669dca52c7fd51472a3f2742f4be16a5738f9b1
confusion between CloseListener and DisposeListener, fixed

desktop/TuxGuitar/src/app/tuxguitar/app/view/dialog/transport/TGTransportModeDialog.java
@@ -22,8 +22,8 @@ import app.tuxguitar.song.models.TGMeasureHeader;
 import app.tuxguitar.song.models.TGSong;
 import app.tuxguitar.song.models.TGTrack;
 import app.tuxguitar.ui.UIFactory;
-import app.tuxguitar.ui.event.UICloseEvent;
-import app.tuxguitar.ui.event.UICloseListener;
+import app.tuxguitar.ui.event.UIDisposeEvent;
+import app.tuxguitar.ui.event.UIDisposeListener;
 import app.tuxguitar.ui.event.UISelectionEvent;
 import app.tuxguitar.ui.event.UISelectionListener;
 import app.tuxguitar.ui.layout.UITableLayout;
@@ -82,9 +82,9 @@ public class TGTransportModeDialog {
 
 		dialog.setLayout(dialogLayout);
 		dialog.setText(TuxGuitar.getProperty("transport.mode"));
-		dialog.addCloseListener(new UICloseListener() {
+		dialog.addDisposeListener(new UIDisposeListener() {
 			@Override
-			public void onClose(UICloseEvent event) {
+			public void onDispose(UIDisposeEvent event) {
 				if ((TGTransportModeDialog.this.colorErr != null) && !TGTransportModeDialog.this.colorErr.isDisposed()) {
 					TGTransportModeDialog.this.colorErr.dispose();
 				}