05a7e8e6484c5344c70df2888d5d428ae523da3c
helge17 <5897631+helge17@users.noreply.github.com>
2026-02-05 21:49:08 +0100

    Allow spaces in the installation path of tuxguitar.sh

--- desktop/build-scripts/common-resources/common-linux/tuxguitar.sh
+++ desktop/build-scripts/common-resources/common-linux/tuxguitar.sh
@@ -12,7 +12,7 @@ getTheme() {
 }
 
 ##SCRIPT DIR
-TG_DIR=`dirname $(realpath "$0")`
+TG_DIR=`dirname "$(realpath "$0")"`
 ##JAVA
 JAVA=`which java`
 ##LIBRARY_PATH
@@ -29,4 +29,4 @@ export LD_LIBRARY_PATH
 ##Avoid problems with Accelerated Compositing mode in SWT/WebKitGTK
 export WEBKIT_DISABLE_COMPOSITING_MODE=1
 ##LAUNCH
-${JAVA} -cp :${CLASSPATH} -Dtuxguitar.home.path="${TG_DIR}" -Dtuxguitar.share.path="share/" -Djava.library.path="${LD_LIBRARY_PATH}" -Dtuxguitar.theme=$(getTheme) ${MAINCLASS} "$@"
+${JAVA} -cp ":${CLASSPATH}" -Dtuxguitar.home.path="${TG_DIR}" -Dtuxguitar.share.path="share/" -Djava.library.path="${LD_LIBRARY_PATH}" -Dtuxguitar.theme=$(getTheme) ${MAINCLASS} "$@"
