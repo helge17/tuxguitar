3d945bd603b983749497e396fc43eb3a3d1e8167
Sumit Soreng <sumitsoreng2000@gmail.com>
2026-08-06 10:29:05 +0530

    Update tests in TestRepeatController for repeat sequences

--- common/TuxGuitar-lib/src/test/java/app/tuxguitar/player/base/TestRepeatController.java
+++ common/TuxGuitar-lib/src/test/java/app/tuxguitar/player/base/TestRepeatController.java
@@ -22,8 +22,8 @@ public class TestRepeatController {
 	@Test
 	public void testNominalSequence() {
 		checkSequence(new MidiRepeatController(this.song, -1, -1),
-				new int[] {1,2,3, 1, 4,5, 1, 4,5, 1, 6,7,  8,9, 8,9, 8,9, 10},
-				new int[] {0,0,0, 3, 1,1, 6, 4,4, 9, 5,5,  5,5, 7,7, 9,9,  9});
+				new int[] {1,2,3, 1, 4,5, 1, 4,5, 1, 6,7,  8,9, 8,9, 8,9, 10,  11,11, 12,12, 13},
+				new int[] {0,0,0, 3, 1,1, 6, 4,4, 9, 5,5,  5,5, 7,7, 9,9,  9,   9,10, 10,11, 11});
 	}
 
 	@Test
@@ -56,10 +56,9 @@ public class TestRepeatController {
 
 	// https://github.com/helge17/tuxguitar/issues/1146
 	@Test
-	public void testConsecutiveRepeats() throws IOException {
-		TGSong customSong = new TestFileFormat20().readSong("consecutiveRepeatsLoop_20.tg", true).getSong();
-		checkSequence(new MidiRepeatController(customSong, 1, 2),
-				new int[] {1,1, 2,2},
+	public void testConsecutiveRepeats() {
+		checkSequence(new MidiRepeatController(this.song, 11, 12),
+				new int[] {11,11, 12,12},
 				new int[] {0,1, 1,2});
 	}
 
