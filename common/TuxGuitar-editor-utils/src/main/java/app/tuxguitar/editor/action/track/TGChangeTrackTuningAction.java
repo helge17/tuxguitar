3a9438b7a5a23f4fd44be8dfe19fd629dac77a49
guiv42 <129443524+guiv42@users.noreply.github.com>
2026-09-14 16:28:59 +0200

    Option to change notes (and keep frets) when changing tuning
    only accessible if the number of strings does not change

--- common/TuxGuitar-editor-utils/src/main/java/app/tuxguitar/editor/action/track/TGChangeTrackTuningAction.java
+++ common/TuxGuitar-editor-utils/src/main/java/app/tuxguitar/editor/action/track/TGChangeTrackTuningAction.java
@@ -17,6 +17,7 @@ public class TGChangeTrackTuningAction extends TGActionBase {
 
 	public static final String ATTRIBUTE_OFFSET = "offset";
 	public static final String ATTRIBUTE_STRINGS = "strings";
+	public static final String ATTRIBUTE_KEEP_FRETS = "change-track-tuning.keep-frets";	// only meaningful if number of strings do not change
 
 	public TGChangeTrackTuningAction(TGContext context) {
 		super(context, NAME);
@@ -30,32 +31,38 @@ public class TGChangeTrackTuningAction extends TGActionBase {
 		if( track != null ) {
 			TGSongManager songManager = getSongManager(context);
 			if( strings != null ){
-				int transposition = 0; // did all strings change by the same value? if so, set variable to a non-zero value
-				if (track.getStrings().size() == strings.size()) {
-					transposition = track.getString(1).getValue() - strings.get(0).getValue();
-					for (int i=1; i<strings.size();i++) {
-						if ((track.getString(i+1).getValue() - strings.get(i).getValue()) != transposition) {
-							transposition = 0;
-							break;
+				boolean keepFrets = Boolean.TRUE.equals(context.getAttribute(ATTRIBUTE_KEEP_FRETS));
+				if (keepFrets && (track.getStrings().size() == strings.size())) {
+					track.setStrings(strings);
+				}
+				else {
+					int transposition = 0; // did all strings change by the same value? if so, set variable to a non-zero value
+					if (track.getStrings().size() == strings.size()) {
+						transposition = track.getString(1).getValue() - strings.get(0).getValue();
+						for (int i=1; i<strings.size();i++) {
+							if ((track.getString(i+1).getValue() - strings.get(i).getValue()) != transposition) {
+								transposition = 0;
+								break;
+							}
 						}
 					}
-				}
-				if (transposition != 0) {
-					// same number of strings, all shifted by the same value
-					int[] transpositions = new int[strings.size()];
-					for (int i=0; i<strings.size();i++) {
-						transpositions[i] = transposition;
+					if (transposition != 0) {
+						// same number of strings, all shifted by the same value
+						int[] transpositions = new int[strings.size()];
+						for (int i=0; i<strings.size();i++) {
+							transpositions[i] = transposition;
+						}
+						track.setStrings(strings);
+						songManager.getTrackManager().transposeNotes(track, transpositions, true, true );
+					} else {
+						// reallocate notes to strings
+						List<Integer> fromStrings = new ArrayList<Integer>();
+						for (int i=1; i<=track.getStrings().size(); i++) {
+							fromStrings.add(track.getString(i).getValue());
+						}
+						track.setStrings(strings);
+						songManager.getTrackManager().allocateNotesToStrings(fromStrings, track, strings);
 					}
-					track.setStrings(strings);
-					songManager.getTrackManager().transposeNotes(track, transpositions, true, true );
-				} else {
-					// reallocate notes to strings
-					List<Integer> fromStrings = new ArrayList<Integer>();
-					for (int i=1; i<=track.getStrings().size(); i++) {
-						fromStrings.add(track.getString(i).getValue());
-					}
-					track.setStrings(strings);
-					songManager.getTrackManager().allocateNotesToStrings(fromStrings, track, strings);
 				}
 			}
 			if( offset != null ) {
